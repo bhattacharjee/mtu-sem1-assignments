84ade5f172bfea09f97f9e149de113d69bcb82ee
Rajbir Bhattacharjee <rajbir.bhattachajee@gmail.com>
2020-10-31 16:42:39 +0000

    Added some more code for validation to be used later

--- MetaheuristicOptimization/Assignment1/tspga.py
+++ MetaheuristicOptimization/Assignment1/tspga.py
@@ -4,20 +4,25 @@ from lab_tsp_insertion import *
 import random, collections, math
 
 class Instance(object):
-    def __init__(self, cities:dict, identifier:int=0):
-        self.cities = cities
-        self.solution, self.solution_cost = insertion_heuristic1(self.cities)
-        self.id = identifier
-        self.solution_cost
-        self.fitness_value = -1
+    def __init__(self, cities:dict, solution:list=None, identifier:int=0):
+        self.cities = self.solution = self.solution_cost = None
+        self.id = self.solution_cost = self.fitness_value = None
+        if (None == solution):
+            self.cities = cities
+            self.solution, self.fitness_value = insertion_heuristic1(self.cities)
+            self.id = identifier
+        else:
+            self.cities = cities
+            self.solution = solution
+            self.fitness_value = -1
+        assert(self.validate())
 
-    """
-    def __init__(self, cities:dict, solution:list, identifier:int=0):
-        self.cities = cities
-        self.solution = solution
-        self.solution_cost = -1
-        self.fitness_value = -1
-    """
+    def validate(self):
+        for i in self.cities.keys():
+            if i not in self.solution:
+                return False
+        duplicates = [item for item, count in collections.Counter(self.solution).items() if count > 1]
+        return [] == duplicates
 
     def __repr__(self):
         return f"{self.id} : {self.solution_cost} <== {self.solution}"
@@ -32,7 +37,6 @@ class Instance(object):
         if -1 != self.fitness_value:
             return self.fitness_value
         self.fitness_value = 0
-        # 1 to N-1
         for i in range(1, len(self.solution)):
             c1, c2 = self.solution[i-1], self.solution[i]
             x1, y1 = self.cities[c1]
@@ -45,6 +49,7 @@ class Instance(object):
         x2, y2 = self.cities[first_city]
         dist = round(math.sqrt((x2-x1)**2 + (y2-y1)**2))
         self.fitness_value += dist
+        assert(self.fitness_value > 0)
         return self.fitness_value
 
 
@@ -52,8 +57,7 @@ class GA(object):
     def __init__(self, cities:dict, population_size:int):
         self.cities = cities
         self.population_size = population_size
-        self.population = []
-        self.population = [Instance(cities,i) for i in range(self.population_size)]
+        self.population = [Instance(cities=cities,identifier=i) for i in range(self.population_size)]
 
     def print_population(self):
         [print(inst) for inst in self.population]
@@ -72,16 +76,7 @@ def main():
     allcities = get_cities(directory)
     ga = GA(allcities, popsize)
     for inst in ga.population:
-        print(inst.fitness(), inst.solution_cost)
-    """
-    for i in range(runs):
-        for filename in os.listdir(directory):
-            cities = readInstance(directory + os.path.sep + filename)
-            print(cities)
-            allcities = {**allcities, **cities}
-            solutionPath, solutionCost = insertion_heuristic1(cities)
-            print(solutionPath, solutionCost)
-    """
+        print(inst.fitness(), inst.fitness_value)
 
 if "__main__" == __name__:
     main()
