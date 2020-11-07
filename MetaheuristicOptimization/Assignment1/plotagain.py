c497d2f461840fab9cf8c4245b0cd3f9c673defe
Rajbir Bhattacharjee <rajbir.bhattachajee@gmail.com>
2020-11-07 20:58:31 +0000

    plot again script and the doc

--- MetaheuristicOptimization/Assignment1/plotagain.py
+++ MetaheuristicOptimization/Assignment1/plotagain.py
@@ -3,7 +3,12 @@
 import matplotlib.pyplot as plt
 import pickle
 import sys
+if len(sys.argv) < 2:
+    print("plotagain.py filename1.pickle [filename2.pickle ...]")
+    sys.exit(1)
 
-ax = pickle.load(file=open(sys.argv[1], "rb"))
+args=sys.argv[1:]
+for plot in args:
+    ax = pickle.load(file=open(plot, "rb"))
 plt.show()
 
