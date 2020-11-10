a90999b3447c3b3d4f96b233777a518695a1d38f
Rajbir Bhattacharjee <rajbir.bhattachajee@gmail.com>
2020-11-10 18:38:09 +0000

    some comments

--- pml/Assignment1/clustering.py
+++ pml/Assignment1/clustering.py
@@ -6,29 +6,74 @@ import argparse
 import sys
 import matplotlib.pyplot as plt
 
+g_apply_scaling = False
+g_apply_normalization = False
+
 def calculate_distances(allvalues:np.ndarray, row:np.ndarray)->np.ndarray:
+    """
+    If there are m features, and 1000 rows, then
+    allValues = 1000 x m            and
+    rows = m x 1
+    """
+
+    """
+    From each row in allvalues, subtract the row
+    Then square the difference
+    This wil be a 1000 x m matrix
+    """
     diff2 = np.square(allvalues - row)
+
+    """
+    For each row in diff2, sum all the columns, then apply sqrt
+    and return.
+    This will be 1000x1
+    """
     return np.sqrt(np.sum(diff2, axis=1))
 
 def read_file(filename:str) -> np.ndarray:
     return np.genfromtxt(filename, dtype=float, delimiter=',')
 
 def generate_centroids(data:np.ndarray, k:int) -> np.ndarray:
+    """
+    Randomly choose k points as initial centroids
+    """
     indices = np.random.choice(data.shape[0], k, replace=False)
     return data[indices,:]
 
 def assign_centroids(data:np.ndarray, centroids:np.ndarray)->list:
     arr = []
+    """
+    For each centroid
+    """
     for centroid in centroids:
+        """
+        Calculate the data between this centroid and each of the data points
+        This will give a kx1000 matrix
+        """
         distances = calculate_distances(data, centroid)
         arr.append(distances)
+    """
+    Append k 1x1000 matrices to make a kx1000 matrix
+    """
     arr = np.array(arr)
+
+    """
+    Find the index of the smallest centroid. If there are k centroids, then
+    the distance of the array is kx1000.
+    For each 1000 columns, find the row i that has the lowest value
+    and that is the index of the smallest centroid, and that is what we assign
+    to this point
+    """
     indices = np.argmin(arr, axis=0)
+
+    """
+    Return a 1000x1 array
+    """
     return indices.T
 
-def calculate_error(data:np.ndarray, centroids:np.ndarray, closest_centroid_indices:np.ndarray)->float:
+def calculate_error(data:np.ndarray, centroids:np.ndarray, assignments:np.ndarray)->float:
     distance = 0
-    closest_centroids = centroids[closest_centroid_indices, :]
+    closest_centroids = centroids[assignments, :]
     square_distances = np.square(np.subtract(data, closest_centroids))
     return np.sum(square_distances) / data.shape[0]
 
@@ -53,18 +98,18 @@ def restart_KMeans(filename:str, num_centroids:int, iterations:int, restarts:int
     best_error = None
     best_assignment = None
     for i in range(restarts):
-        error, closest_centroid_indices = calculate_clusters(np.copy(data), num_centroids, iterations)
+        error, assignments = calculate_clusters(np.copy(data), num_centroids, iterations)
         if None == best_error or error < best_error:
             best_error = error
-            best_assignment = closest_centroid_indices
+            best_assignment = assignments
     return best_error, best_assignment
 
 def restart_and_elbow_plot(filename:str, iterations:int, restarts:int, max_N:int):
     x = []
     y = []
-    for i in range(1, max_N+1):
+    for i in range(3, max_N+1):
         error, assignment = restart_KMeans(filename, i, iterations, restarts)
-        #print(i, error)
+        print(i, error)
         x.append(i)
         y.append(error)
     plt.plot(x, y)
@@ -74,4 +119,4 @@ if "__main__" == __name__:
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--file", help="File name", type=str, required=True)
     args = parser.parse_args()
-    restart_and_elbow_plot(args.file, 50, 5, 10)
+    restart_and_elbow_plot(args.file, 200, 10, 10)
