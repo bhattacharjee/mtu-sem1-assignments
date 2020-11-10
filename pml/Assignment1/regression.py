16bc63ee78280100bd86682a8aacf8810919ef15
Rajbir Bhattacharjee <rajbir.bhattachajee@gmail.com>
2020-11-10 19:26:03 +0000

    Some refactoring

--- pml/Assignment1/regression.py
+++ pml/Assignment1/regression.py
@@ -1,23 +1,29 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 
 import numpy as np
 import argparse
 
-g_normalize_to_zero_mean_and_unit_variance = False
+g_normalize_to_zero_mean_and_unit_variance = True
 g_scale_between_zero_and_one = True
 
 def read_csv(filename:str)->np.ndarray:
     return np.genfromtxt(filename, dtype=float, delimiter=',')
 
 
+
 def calculate_distances(allvalues:np.ndarray, row:np.ndarray)->np.ndarray:
     diff2 = np.square(allvalues - row)
     return np.sqrt(np.sum(diff2, axis=1))
+
+
 
 def get_min_max(allvalues:np.ndarray)->tuple:
     amin = np.amin(allvalues, axis=0)
     amax = np.amax(allvalues, axis=0)
     return amin, amax
+
+
+
 
 def normalize(array:np.ndarray, stdarray:np.ndarray, meanarray:np.ndarray):
     global g_normalize_to_zero_mean_and_unit_variance
@@ -26,6 +32,9 @@ def normalize(array:np.ndarray, stdarray:np.ndarray, meanarray:np.ndarray):
         return x
     else:
         return array
+
+
+
 
 def scale(array:np.ndarray, amin:np.ndarray, amax:np.ndarray)->np.ndarray:
     global g_scale_between_zero_and_one
@@ -36,16 +45,29 @@ def scale(array:np.ndarray, amin:np.ndarray, amax:np.ndarray)->np.ndarray:
     else:
         return array
 
+
+
+
 def predict(train_features:np.ndarray, train_values:np.ndarray, test_features:np.ndarray, k:int, n:int)->np.ndarray:
-    stddvarr = np.std(train_features, axis=0)
-    meanarr = np.mean(train_features, axis=0)
-    train_norm = normalize(train_features, stddvarr, meanarr) # Normalize
-    amin, amax = get_min_max(train_norm)
-    train_norm = scale(train_norm, amin, amax)                # Scale
-    stddev = np.std(train_norm, axis=0)
-    test_norm = normalize(test_features, stddvarr, meanarr)
-    test_norm = scale(test_norm, amin, amax)        # Normalize
-    test_norm = scale(test_features, amin, amax)    # Scale
+    global g_normalize_to_zero_mean_and_unit_variance, g_scale_between_zero_and_one
+    if g_normalize_to_zero_mean_and_unit_variance:
+        stddvarr = np.std(train_features, axis=0)
+        meanarr = np.mean(train_features, axis=0)
+        train_norm = normalize(train_features, stddvarr, meanarr) # Normalize
+    else:
+        train_norm = train_features
+
+    if g_scale_between_zero_and_one:
+        amin, amax = get_min_max(train_norm)
+        train_norm = scale(train_norm, amin, amax)                # Scale
+
+    if g_normalize_to_zero_mean_and_unit_variance:
+        test_norm = normalize(test_features, stddvarr, meanarr)
+    else:
+        test_norm = test_features
+
+    if g_scale_between_zero_and_one:
+        test_norm = scale(test_norm, amin, amax)        # Normalize
 
 
     all_predictions = []
@@ -65,14 +87,17 @@ def predict(train_features:np.ndarray, train_values:np.ndarray, test_features:np
 
     return all_predictions
 
+
+
+
 def calculate_r2(predicted:np.ndarray, actual:np.ndarray)->float:
     sum_square_residuals = np.sum(np.square(actual - predicted))
     mean_actual = np.mean(actual)
     sum_squares = np.sum(np.square(actual - mean_actual))
     return 1 - (sum_square_residuals / sum_squares)
 
-    
-    
+
+
 def main(filename:str, testfilename=str):
     array = read_csv(filename)
     test = read_csv(testfilename)
@@ -84,6 +109,7 @@ def main(filename:str, testfilename=str):
 
 
     for n in range(1, 20, 1):
+        # Best values are k=10, n=5
         predicted = predict(train_features, train_labels, test_features, 3, n)
         r2 = calculate_r2(predicted, test_labels)
         print(n, r2)
