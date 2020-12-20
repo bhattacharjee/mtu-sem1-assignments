2c972049f0b4781113fdccd9f285c97098dae9d3
Rajbir Bhattacharjee <rajbir.bhattachajee@gmail.com>
2020-12-20 17:32:12 +0000

    checkpoint

--- MetaheuristicOptimization/Assignment2/run_nocache.sh
+++ MetaheuristicOptimization/Assignment2/run_nocache.sh
@@ -1,5 +1,10 @@
 #!/bin/ksh93
 
+# Usage run_nocache.sh <algorithm>
+# where algorithm is either 0, 1 or 2 to indicate
+# basic 2opt, variant 1 (random edge) or variant 2 (random edge with first
+# improving combination)
+
 #for FILENAME in small/inst-0.tsp
 ALG=$1
 #for ALG in 2 1 0
