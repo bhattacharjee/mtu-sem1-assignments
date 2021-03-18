993e6f61474027f445207e63f4f99118defbfb47
Rajbir Bhattacharjee <rajbir.bhattachajee@gmail.com>
2021-03-18 22:57:29 +0000

    checkpointing

--- machine-vision/a1/aa2.py
+++ machine-vision/a1/aa2.py
@@ -9,11 +9,15 @@ import cv2
 import math
 import argparse
 
-from Lab_MV_04_Interest_points import *
+#from Lab_MV_04_Interest_points import *
 
+# This is really not used
 GAUSSIAN_ADD = 0
+
+# The threshold value for non-maxima suppression
 THRESHOLD = 10
 
+# This will be overwritten by argument parser
 g_filename = 'Assignment_MV_1_image.png'
 
 """
@@ -50,8 +54,11 @@ def display_image(i:np.ndarray)->None:
 Given an array of images, show all the images in the same window.
 There are additional arrays which contains the titles for each image
 """
-def display_images_meshgrid(images:list, gray=True, \
-        title_arr=None, suptitle=None)->None:
+def display_images_meshgrid(\
+        images:list,\
+        gray=True, \
+        title_arr=None,\
+        suptitle=None)->None:
     """
     Display multiple images side by side
     Images are in form of a list of np.ndarray
@@ -88,7 +95,7 @@ GAUSSIAN_KERNEL_SIZE_MULTIPLIER = 3
 """
 Given a sigma, return a gaussian smoothing kernel of the appropriate size
 """
-def get_gaussian_smoothing_kernel(sigma:float):
+def get_gaussian_smoothing_kernel(sigma:float)->np.ndarray:
     p_x, p_y = np.meshgrid(\
            np.arange(-GAUSSIAN_KERNEL_SIZE_MULTIPLIER * sigma, \
                 GAUSSIAN_KERNEL_SIZE_MULTIPLIER * sigma),\
@@ -100,7 +107,7 @@ def get_gaussian_smoothing_kernel(sigma:float):
 Get 12 gaussian smoothing kernels with different sigmas
 where sigma = (2 ** (k/2)), k = 0,1,...,11
 """
-def get_twelve_smoothed_images(image)->tuple:
+def get_twelve_smoothed_images(image:np.ndarray)->tuple:
     sigma_arr = []
     images = []
     for k in range(12):
@@ -114,7 +121,8 @@ def get_twelve_smoothed_images(image)->tuple:
 """
 Get difference of gaussian images, with different sigma values
 """
-def get_difference_of_gaussian_images(images, sigma_arr)->list:
+def get_difference_of_gaussian_images(images:np.ndarray,\
+        sigma_arr:list)->list:
     dog_arr = []
     for i in range(len(images)-1):
         i1, i2 = images[i], images[i+1]
@@ -137,7 +145,13 @@ This is because this will be called thrice for each layer,
 
 For 1, check_same_level=False and True otherwise
 """
-def is_maximal_pixel(metric, x, y, pixel_value, T, check_same_level=True):
+def is_maximal_pixel(\
+        metric:np.ndarray,\
+        x:int,\
+        y:int,\
+        pixel_value:float,\
+        T:int,\
+        check_same_level:bool=True)->bool:
     try:
         if ((pixel_value > T) and
             (pixel_value > metric[x-1,y-1]) and
@@ -165,7 +179,12 @@ For a particular level in scale-space, get all non-maxima suppressed pixels
 at that level.
 This is done by calling is_maximal_pixel for every pixel
 """
-def get_non_maxima_suppression_pixels(image, lower, higher, T, sigma):
+def get_non_maxima_suppression_pixels(\
+        image:np.ndarray,\
+        lower:np.ndarray,\
+        higher:np.ndarray,\
+        T:int,\
+        sigma:list)->list:
     points = []
     for x in range(1,len(image)-1):
         for y in range(1,len(image[0])-1):
@@ -186,7 +205,7 @@ def get_non_maxima_suppression_pixels(image, lower, higher, T, sigma):
 """
 Get all keypoints for all levels in scale space
 """
-def get_all_non_maxima_suppression_pixels(dog_arr, T):
+def get_all_non_maxima_suppression_pixels(dog_arr:list, T:int)->list:
     points = []
     for i in range(len(dog_arr)):
         lower = higher = image = None
@@ -226,6 +245,7 @@ def task_1b():
         gaussian_kernels.append(get_gaussian_smoothing_kernel(s))
     display_images_meshgrid(gaussian_kernels, \
             gray=False, title_arr=title_arr, suptitle="gaussian kernels")
+    plt.show()
 
 """
 1. Gets the scale space representation with the gaussian kernel at different
@@ -239,7 +259,11 @@ def task_2()->list:
     save_image = load_original_image()
     images, sigma_arr = get_twelve_smoothed_images(image)
 
+    # This variable controls whether the original image is considered in the
+    # scale space or not.
+    # For now we set it to false
     include_base_image = False
+
     if include_base_image:
         new_images = [image.copy()]
         [new_images.append(x) for x in images]
@@ -249,7 +273,11 @@ def task_2()->list:
         new_images = images
         new_sigma_arr = sigma_arr
 
+    # Get the difference of Gaussian by calling this function
     dog_arr = get_difference_of_gaussian_images(new_images, new_sigma_arr)
+
+    # Now display all the difference of Gaussian images. Most of the code here
+    # Is to get the right title appended to each dog
     display = []
     display_text = []
     for x in dog_arr:
@@ -258,19 +286,28 @@ def task_2()->list:
         display.append(dog)
     display_images_meshgrid(display,\
             True, display_text, "Difference of Gaussian")
+    plt.show()
+
+    # Get all key points by non-maxima suppression
     points = get_all_non_maxima_suppression_pixels(dog_arr,\
                 GAUSSIAN_ADD+THRESHOLD)
     print(f"Number of points found = {len(points)}")
+
+    # For each of the key points, draw a circle
     for point in points:
         x, y, sigma = point
         x = int(x)
         y = int(y)
         radius = math.floor(3 * sigma)
         cv2.circle(save_image, (y, x,), radius, (0,255,0), 1)
+
+    # Display the image, and also save it to a file
     cv2.imwrite("circles_only.jpg", save_image)
     cv2.imshow("result", save_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+    # Return the key points, this will be used by task 3
     return points
 
 """
@@ -295,7 +332,7 @@ of each point. This function gives us the weight of each point.
 LRU cache is used to memoize the result once it is calculated
 """
 @lru_cache
-def get_weight(q, r, sigma):
+def get_weight(q:int, r:int, sigma:float)->float:
     wqr = np.exp( -(q**2 + r**2) / (9 * sigma**2 / 2 ))
     wqr = wqr / (9 * np.pi * sigma**2 / 2)
     return wqr
@@ -308,8 +345,15 @@ and then appends the angle to each key point
 The magnitude and angle have already been calculated for all the points
 and saved so that they needn't be calculated again and again
 """
-def get_augmented_point_with_direction(im, x, y, sigma, m_gx, m_gy, m_theta,\
-                                        m_magnitude):
+def get_augmented_point_with_direction(\
+        im:np.ndarray,\
+        x:int,\
+        y:int,\
+        sigma:np.ndarray,\
+        m_gx:np.ndarray,\
+        m_gy:np.ndarray,\
+        m_theta:np.ndarray,\
+        m_magnitude:np.ndarray)->tuple:
     hist = [0.0] * 36
     for k in range(-3, 4):
         for j in range(-3, 4):
@@ -328,7 +372,7 @@ def get_augmented_point_with_direction(im, x, y, sigma, m_gx, m_gy, m_theta,\
 """
 Display derivative of gaussian kernels
 """
-def show_derivative_of_gaussian_images(images):
+def show_derivative_of_gaussian_images(images:list)->None:
     gxa = []
     gya = []
     for dog in images:
@@ -341,6 +385,9 @@ def show_derivative_of_gaussian_images(images):
 
 """
 Display derivative of gaussian kernels
+
+The cross-ref we create in task_3() comes in very handy as we can just
+lookup anything that we have already computed earlier in O(1) time.
 """
 def plot_gx_gy(sigma_cross_ref:dict)->None:
     title_arr = []
@@ -354,11 +401,20 @@ def plot_gx_gy(sigma_cross_ref:dict)->None:
     display_images_meshgrid(gy_arr, True, title_arr, "gy")
 
 
-def task_3():
+def task_3()->tuple:
+    # Get the grayscale image
     image = load_image_and_convert_to_grayscale()
+    # Also load the original color image, which we will use
+    # to draw everything at the end
     save_image = load_original_image()
+
+    # Get the twelve smoothed images
     images, sigma_arr = get_twelve_smoothed_images(image)
+
+    # Get the key points from task 2, this will be augmented by the direction
     key_points = task_2()
+
+    # Get the derivative of gaussian images in x and y direction
     dog_images = get_derivative_of_gaussian(images, sigma_arr)
     magnitudes = []
     theta_array = []
@@ -366,6 +422,11 @@ def task_3():
 
     # Calculate the magnitude and angle for all points
     # We will select the points that matter to us later
+    # This cross reference allows us to have a quick way to access
+    # stuff that we've already calculated
+    # task 4 will also use this cross ref, so we're calculating
+    # some stuff here that is not strictly required in this task
+    # as they will be required in task 4
     for im, sigma, (gx, gy) in zip(images, sigma_arr, dog_images):
         magnitude = np.sqrt(np.square(gx) + np.square(gy))
         magnitudes.append(magnitude)
@@ -381,8 +442,12 @@ def task_3():
                 "magnitude": magnitude
                 }
 
-    plot_gx_gy(sigma_cross_ref)
 
+    # Now plot the derivative of gaussian in x and y directions
+    plot_gx_gy(sigma_cross_ref)
+    plt.show()
+
+    # For each key point, find the direction and augment that point
     augmented_points = []
     exception_count = 0
     for point in key_points:
@@ -400,6 +465,9 @@ def task_3():
         except:
             exception_count += 1
             
+    # Now that we have found the direction theta for each point,
+    # Draw a circle and a radius in the direction that we just found out
+    # Also save the window
     save_image = load_original_image()
     for point in augmented_points:
         x, y, sigma, angle = point
@@ -417,45 +485,78 @@ def task_3():
 
     return augmented_points, sigma_cross_ref
 
+"""The magnitude at each point will be multiplied by this weight in
+task 4
+"""
 @lru_cache
-def get_weight2(s, t, sigma):
+def get_weight2(s:int, t:int, sigma:float)->float:
     return np.exp(-(s**2 + t**2) / (81 * sigma**2 /2)) / \
             (81 * np.pi * sigma**2 / 2)
 
-def task_4():
+def task_4()->None:
+    # Get the key points augmented with the direction of each point,
+    # as well as the sigma cross reference of all the stuff we've already
+    # calculated at that sigma in task 2
     augmented_points, sigma_crf = task_3()
+
+    # This array will hold all the SIFT descriptors for all the points
     all_descriptors = []
+
+    # Now iterate all the points and find out the SIFT descriptor for each
+    # point
     for point in augmented_points:
         try:
             x, y, sigma, theta_hat = point
             theta_arr = sigma_crf[sigma]["theta"]
             magnitude_arr = sigma_crf[sigma]["magnitude"]
             all_hist = []
+
+            # Break the pixels around the key point into 16 4x4 grids
+            # two nested loops to form 16 4x4 grids
             for i in range(-2, 2):
                 for j in range(-2, 2):
+
+                    # These will be used to calculate the coordinates inside
+                    # each 4x4 grid
                     kk1 = [4 * i + x for x in range(4)]
                     kk2 = [4 * j + x for x in range(4)]
 
                     # This is the orientation histogram for each 4x4 subgrid
                     hist = [0.0] * 8 
 
+                    # For each of the pixels in the 4x4 grid
                     for k1 in kk1:
                         for k2 in kk2:
+
                             s = int(round(9/16 * (k1 + 0.5) * sigma))
                             t = int(round(9/16 * (k2 + 0.5) * sigma))
                             wst = get_weight2(s, t, sigma)
                             xs = x + s
                             yt = y + t
+
+                            # Find the magnitude
                             mst = magnitude_arr[xs, yt]
+
+                            # Find the angle and subtract theta_hat from it
                             theta_st = theta_arr[xs, yt] - theta_hat
+
+                            # Find the index in the histogram for this
+                            # 4x4 subgrid from the angle
                             ind = int(math.floor(theta_st / (2 * np.pi / 8)))
+
+                            # Update the weight in that index of the histogram
                             hist[ind] += (wst * mst)
 
                     # Now that we're done calculating the histogram for this
                     # 4x4 sub-grid, append it to the list of histograms for
                     # this point to form the descriptor
+                    # At the end of all loops for this point, this will have
+                    # 128 floats
                     all_hist.extend(hist)
 
+            # append the descriptors for this point into the global list
+            # of descriptors.
+            # Each descriptor for each point has 128 floats
             all_hist = np.array(all_hist)
             all_hist = all_hist / np.sqrt(np.sum(np.square(all_hist)))
             all_hist[all_hist > 0.2] = 0.2
